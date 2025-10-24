ab9f61003b48306258f40a971e35172d2674b7e9
Uzair <programmeruzair@gmail.com>
2025-10-25 00:17:37 +0530

    Implement login and logout functionality in auth routes

--- backend/src/controllers/signup.controller.js
+++ backend/src/controllers/signup.controller.js
@@ -64,3 +64,41 @@ export const signup = async (req, res) => {
     }
 
 }
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email.trim() || !password.trim()){
+        return res.status(400).json({ message: "All feilds are required" })
+    }
+
+    try {
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({ message: "Invalid Credentials" })
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) return res.status(400).json({ message: "Invalid Credentials" })
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        })
+    } catch (error) {
+        console.log("Error in login controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const logout = (_, res) =>{
+    res.cookie("jwt", "", {
+        maxAge: 1,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: ENV.NODE_ENV === "development" ? false : true,
+    });
+    res.status(200).json({ message: "Logged out successfully" });
+}
